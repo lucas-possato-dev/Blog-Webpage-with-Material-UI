b42047ffdf6338bd73731d5b8fef27eb6f5e731e
lucas-possato-dev <lucaspstinho@gmail.com>
2023-07-04 13:13:12 -0300

    fixing import on drawer

--- src/Components/Drawer.jsx
+++ src/Components/Drawer.jsx
@@ -10,7 +10,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
-import { sections } from "../Data/Data";
+import { sections } from "../data/Data";
 
 export default function SideDrawer({ children }) {
   const [state, setState] = React.useState({
